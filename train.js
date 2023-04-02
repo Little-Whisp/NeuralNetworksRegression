76a96a01a76b841944ea5064d0f404b942757258
Jany <slmartis@outlook.com>
2023-04-02 23:20:04 +0200

    Commit (02-04-2023)

--- train.js
+++ train.js
@@ -1,9 +1,16 @@
-import { createChart, updateChart } from "./scatterplot.js"
+import { createChart, updateChart} from "./scatterplot.js"
+const nn = ml5.neuralNetwork({ task: 'regression', debug: true })
+let trainData;
+let testData;
 
-const nn = ml5.neuralNetwork({task: 'regression', debug: true})
-// nn.load('./model/model.json', modelLoaded)
-// let trainData
-// let testData
+//Buttons
+const predictButton = document.getElementById("predict");
+const saveButton = document.getElementById("save");
+const result = document.getElementById("result");
+
+saveButton.addEventListener("click", () => save());
+predictButton.addEventListener("click", () => makePrediction());
+
 
 function loadData(){
         Papa.parse("./data/winequality.csv", {
@@ -11,53 +18,52 @@ function loadData(){
                 header:true,
                 dynamicTyping:true,
                 complete: results => checkData(results.data)
-        })
-}loadData()
+        });
+}
 
-
-//Zet de ph en alchohol om naar x en y waarden.
+//Zet de ph en alcohol om naar x en y waarden.
 function checkData(data){
+        // data voorbereiden
+        data.sort(() => (Math.random() - 0.5))
+        trainData = data.slice(0, Math.floor(data.length * 0.8))
+        testData = data.slice(Math.floor(data.length * 0.8) + 1)
+        console.table(data)
+
+        // data toevoegen aan neural network
+        for (let wine of trainData) {
+                nn.addData({ quality: wine.quality, citricAcid: wine.citricAcid, residualSugar: wine.residualSugar}, {alcohol: wine.alcohol})
+        }
+
+        nn.normalizeData()
+
         const chartdata = data.map(wine => ({
                 x: wine.quality,
-                y: wine.alchohol,
+                y: wine.alcohol,
         }))
 
         // chartjs aanmaken
-        createChart(chartdata, "Quality", "Alchohol")
+        createChart(chartdata, "Quality", "Alcohol");
 
-        // data voorbereiden
-        data.sort(() => (Math.random() - 0.5))
-        let trainData = data.slice(0, Math.floor(data.length * 0.8))
-        let testData = data.slice(Math.floor(data.length * 0.8) + 1)
-
-        // data toevoegen aan neural network
-        for (let wine of data) {
-                nn.addData({ quality: wine.quality, citricAcid: wine.citricAcid, residualSugar: wine.residualSugar}, {alchohol: wine.alchohol})
-        }
-        nn.normalizeData()
-        nn.train({ epochs: 10 }, () => finishedTraining())
+        nn.train({ epochs: 10 }, () => finishedTraining());
 }
 
-
 async function finishedTraining() {
-        let predictions = []
-        for (let quality = 40; quality < 250; quality += 2) {
-                const pred = await nn.predict({quality: quality})
-                predictions.push({x: quality, y: pred[0].alchohol})
-        }
-        updateChart("Predictions", predictions)
+       console.log("Finished");
 }
 
 async function makePrediction() {
-        const testCar = { quality: testData[0].quality, citricAcid: testData[0].citricAcid, residualSugar:testData[0].residualSugar }
-        const pred = await nn.predict(testCar)
-        console.log(pred[0].alchohol)
+
+        let wineQuality = document.getElementById('quality').value;
+        let wineCitricAcid = document.getElementById('citricAcid').value;
+        let residualSugar = document.getElementById('residualSugar').value;
+
+        const results = await nn.predict({ quality:parseInt(wineQuality), CitricAcid:parseInt(wineCitricAcid), residualSugar:parseInt(residualSugar) })
+        result.innerText = `How much alcohol: ${(results[0].alcohol)}`
 }
 
-async function drawPredictions() {
-        let predictions = []
-        for (let quality = 40; quality <= 250; quality += 5) {
-                const prediction = await nn.predict({quality: ph})
-                predictions.push({x: quality, y: prediction[0].alchohol})
-        }    updateChart("Predictions", predictions)
+function save(){
+        nn.save();
 }
+
+loadData()
+
